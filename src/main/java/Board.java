088e0836f1656573dc3cf802f4fa620f3b413199
zlatica-pernar <129206741+zlatica-pernar@users.noreply.github.com>
2023-12-30 17:19:14 +0100

    Class Board: implemented methods isCellEmpty, place, isFull, clear. Class TicTacToe: basic functionalities for user story 1 implemented (start method) added JUnit config/dependencies to pom.xml file + Unit Tests for all implemented methods + documentation

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,26 +1,69 @@
+/**
+ * Represents the game board for Tic-Tac-Toe.
+ * The board is a 3x3 grid where players can place their markers ('X' or 'O').
+ */
 public class Board {
+
+    /** The 3x3 grid to store markers ('X' or 'O'). */
     protected char[][] cells = new char[3][3];
 
+    /**
+     * Constructs a new empty board and initializes it by calling the clear method.
+     */
     public Board() {
         clear();
     }
 
+    /**
+     * Checks if a specific cell on the board is empty.
+     *
+     * @param x The row index of the cell.
+     * @param y The column index of the cell.
+     * @return true if the cell is empty, false otherwise.
+     */
     public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == '\u0000';
+    }
+
+    /**
+     * Places a marker on the specified cell if the cell is empty.
+     *
+     * @param x      The row index of the cell.
+     * @param y      The column index of the cell.
+     * @param marker The marker to be placed ('X' or 'O').
+     */
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+        }
+    }
+
+    /**
+     * Checks if the board is full, i.e., if all cells are occupied.
+     *
+     * @return true if the board is full, false otherwise.
+     */
+    public boolean isFull() {
+        for (char[] row : cells) {
+            for (char cell : row) {
+                if (cell == '\u0000') {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
-    public void place(int x, int y, char marker) {
-
-    }
-
-    public boolean isFull() {
-              return true;
-    }
-
+    /**
+     * Clears the entire board by setting all cells to an empty state.
+     */
     public void clear() {
-
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = '\u0000';
+            }
+        }
     }
-
     public void print() {
 
     }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,13 +1,29 @@
+/**
+ * The Player class represents a player in a Tic-Tac-Toe game.
+ * Each player is associated with a marker ('X' or 'O') on the game board.
+ */
 public class Player {
 
+    /** The marker (symbol) assigned to the player ('X' or 'O'). */
     protected char marker;
 
+    /**
+     * Constructs a new Player instance with the specified marker.
+     *
+     * @param marker The marker assigned to the player ('X' or 'O').
+     */
     public Player(char marker) {
         this.marker = marker;
     }
 
+    /**
+     * Gets the marker assigned to the player.
+     *
+     * @return The marker ('X' or 'O') associated with the player.
+     */
     char getMarker() {
         return marker;
     }
 }
+
 
