bae19d6c1979506a976843d5970819bdc6a8df69
zlatica-pernar <129206741+zlatica-pernar@users.noreply.github.com>
2024-01-01 17:35:20 +0100

    added hasWinner() method + implementation in start() method + Unit Tests for hasWinner()

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -4,12 +4,19 @@ public class TicTacToe {
     private Player player1;
     private Player player2;
     private Player currentPlayer;
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
+
     private Board board;
     public Player getCurrentPlayer() {
         return currentPlayer;
     }
-    public static void main(String[] args) {
 
+    public Board getBoard() {
+        return board;
     }
 
     public TicTacToe() {
@@ -18,6 +25,7 @@ public class TicTacToe {
         player2 = new Player('O');
         currentPlayer = player1;
     }
+
     public void start() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Tic Tac Toe Game Started!");
@@ -45,13 +53,69 @@ public class TicTacToe {
                 } else {
                     System.out.println("Invalid input. Try again.");
                 }
+                // Check for win or draw
+                if (hasWinner()) {
+                    gameEnded = true;
+                    // Print the winner's board state
+                    System.out.println("Current Player: " + currentPlayer.getMarker());
+                    board.print();
+                    System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+                } else if (board.isFull()) {
+                    gameEnded = true;
+                    board.print();
+                    System.out.println("The game ended in a draw!");
+                } else {
+                    // Switch the current player
+                    switchCurrentPlayer();
+                }
             }
         }
     }
+    /**
+     * Switches the current player in the Tic-Tac-Toe game.
+     *
+     * The method toggles between two players, changing the current player from player1
+     * to player2 or vice versa. This allows the game to alternate turns between the
+     * two players during each move.
+     */
     public void switchCurrentPlayer() {
         currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
+
+    /**
+     * Checks if there is a winner in the Tic-Tac-Toe game.
+     *
+     * The method examines the rows, columns, and diagonals of the game board to determine
+     * if a player has achieved a winning combination.
+     *
+     * @return true if there is a winner, false otherwise
+     */
     public boolean hasWinner() {
+        char[][] cells = board.cells;
+
+        // Check rows for a win
+        for (int i = 0; i < 3; i++) {
+            if (cells[i][0] != '\u0000' && cells[i][0] == cells[i][1] && cells[i][0] == cells[i][2]) {
+                return true;
+            }
+        }
+
+        // Check columns for a win
+        for (int j = 0; j < 3; j++) {
+            if (cells[0][j] != '\u0000' && cells[0][j] == cells[1][j] && cells[0][j] == cells[2][j]) {
+                return true;
+            }
+        }
+
+        // Check diagonals for a win
+        if (cells[0][0] != '\u0000' && cells[0][0] == cells[1][1] && cells[0][0] == cells[2][2]) {
+            return true;
+        }
+        if (cells[0][2] != '\u0000' && cells[0][2] == cells[1][1] && cells[0][2] == cells[2][0]) {
+            return true;
+        }
+
+        // No winner found
         return false;
     }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -25,6 +25,49 @@ public class TicTacToeTest {
         // Assert that the player has been switched
         assertNotEquals(initialPlayer, switchedPlayer);
     }
+
+    /**
+     * Test case: hasWinner should return true if Winner is found.
+     */
+    @Test
+    public void hasWinner_shouldReturnTrueForWinner(){
+        // Arrange
+
+        TicTacToe ticTacToe = new TicTacToe();
+        Board board = ticTacToe.getBoard();
+
+        // Set up a winning scenario in a row
+        board.cells[0][0] = 'X';
+        board.cells[0][1] = 'X';
+        board.cells[0][2] = 'X';
+
+        // Act and Assert
+        assertTrue(ticTacToe.hasWinner());
+    }
+
+    /**
+     * Test case: hasWinner should return false if Game ends in a draw.
+     */
+    @Test
+    public void hasWinner_shouldReturnFalseForNoWinner() {
+        // Arrange
+        TicTacToe ticTacToe = new TicTacToe();
+        Board board = ticTacToe.getBoard();
+
+        // Set up a scenario with no winner
+        board.cells[0][0] = 'X';
+        board.cells[0][1] = 'O';
+        board.cells[0][2] = 'X';
+        board.cells[1][0] = 'X';
+        board.cells[1][1] = 'X';
+        board.cells[1][2] = 'O';
+        board.cells[2][0] = 'O';
+        board.cells[2][1] = 'X';
+        board.cells[2][2] = 'O';
+
+        // Act and Assert
+        assertFalse(ticTacToe.hasWinner());
+    }
 }
 
 
