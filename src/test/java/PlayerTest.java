f3f7040fa815456b10de65e337701da23a47c68e
valentinaskn <valentina.saykin@gmail.com>
2024-01-05 01:05:14 +0100

    Added detailed game instructions to start method

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -8,21 +8,19 @@ public class PlayerTest {
 
     /**
      * Test that verifies the getMarker method returns the correct marker.
-     * This is a positive test case where we expect to get the exact marker
-     * that was set for the player.
+     *
      */
     @Test
     public void testGetMarkerPositive() {
         char expectedMarker = 'X';
         Player player = new Player(expectedMarker);
         char actualMarker = player.getMarker();
-        assertEquals("getMarker should return the correct marker 'X'.", expectedMarker, actualMarker);
+        assertEquals(expectedMarker, actualMarker);
     }
 
     /**
      * Test that verifies the getMarker method does not return a marker
-     * that wasn't set for the player. This is a negative test case to ensure
-     * that the getMarker method does not return incorrect data.
+     * other than the one we set for the player.
      */
     @Test
     public void testGetMarkerNegative() {
@@ -30,6 +28,6 @@ public class PlayerTest {
         char notExpectedMarker = 'X'; // This is not the marker we set for the player
         Player player = new Player(setMarker);
         char actualMarker = player.getMarker();
-        assertNotEquals("getMarker should not return a marker other than 'O'.", notExpectedMarker, actualMarker);
+        assertNotEquals(notExpectedMarker, actualMarker);
     }
 }
